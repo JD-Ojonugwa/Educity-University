7f6f7e04c392f840fcdea275aa38607383813890
JD <joydorothy222@gmail.com>
2024-05-09 23:27:19 +0100

    feat: created navbar with scroll effect (install react scroll)

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from "./Component/Navbar/Navbar";
+// import Hero from "./Component/Hero/Hero";
+// import About from "./Component/About/About";
+// import Title from "./Component/Title/Title";
+// import Program from "./Component/Program/Program";
+// import Campus from "./Component/Campus/Campus";
+// import Testimonials from "./Component/Testimonials/Testimonials";
+// import Contact from "./Component/Contact/Contact";
+// import Footer from "./Component/Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* <Hero />
+      <div className="container">
+        <Title subTitle="Our PROGRAM" title="What we offer" />
+        <Program />
+        <About />
+        <Title subTitle="GALLERY" title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS" title="What our Student Says" />
+        <Testimonials />
+        <Title subTitle="CONTACT US" title="Get in Touch" />
+        <Contact />
+        <Footer />
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
